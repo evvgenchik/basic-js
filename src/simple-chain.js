8986999ba8ea49ccaf7d43f0c85973fc001019c6
Evgeniy <106548359+evvgenchik@users.noreply.github.com>
2022-10-15 22:27:39 +0300

    feat: solve chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -4,29 +4,147 @@ const { NotImplementedError } = require('../extensions/index.js');
  * Implement chainMaker object according to task description
  * 
  */
+
+
+
+// const chainMaker = {
+
+// 	chain: [],
+
+// 	changeChainForRemove() {
+// 		let linkForRemove = this.chain[0]
+
+// 		let value = parseInt(linkForRemove.match(/\d+/))
+
+// 		this.chain.splice(0, 1);
+
+// 		this.chain.unshift(`( ${value} )`)
+// 	},
+
+// 	changeChainForReverse() {
+// 		let newArr = [];
+
+// 		for (let item of this.chain) {
+// 			newArr.push(item.match(/\d+/))
+// 		};
+
+// 		this.chain = [];
+
+// 		for (let item of newArr) {
+// 			this.addLink(item)
+// 		}
+// 	},
+
+// 	getLength() {
+// 		return this.chain.length
+// 	},
+
+// 	addLink(value) {
+// 		if (!value) {
+// 			value = ''
+// 		}
+
+// 		if (this.chain.length === 0) {
+// 			this.chain.push(`( ${value} )`)
+// 			return this
+// 		} else {
+// 			this.chain.push(`~~( ${value} )`)
+// 		}
+// 		return this
+// 	},
+
+// 	removeLink(position) {
+// 		let index = (this.chain.findIndex(item => item == `~~( ${position} )`) != -1) ? this.chain.findIndex(item => item == `~~( ${position} )`) : (this.chain.findIndex(item => item == `( ${position} )`));
+
+// 		if (!Number.isInteger(position) || index == -1) {
+// 			throw Error("You can't remove incorrect link!")
+// 		}
+
+// 		this.chain.splice(index, 1);
+
+// 		if (index === 0) {
+// 			this.changeChainForRemove()
+// 		}
+// 	},
+
+// 	reverseChain() {
+// 		this.chain.reverse()
+// 		this.changeChainForReverse()
+// 	},
+
+// 	finishChain() {
+// 		return this.chain.join('')
+// 		this.chain = [];
+// 	}
+// };
 const chainMaker = {
-  getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+
+	chain: [],
+
+	getLength() {
+		return this.chain.length
+	},
+
+	addLink(value) {
+
+		if (value === "") {
+			value = ''
+		}
+
+		this.chain.push(value)
+
+		return this
+	},
+
+	removeLink(position) {
+		let index = position - 1;
+
+		if (!Number.isInteger(position) || position <= 0 || position > this.chain.length) {
+			this.chain = [];
+			throw Error("You can't remove incorrect link!")
+		}
+
+		this.chain.splice(index, 1);
+
+		return this
+	},
+
+	reverseChain() {
+		this.chain.reverse()
+
+		return this
+	},
+
+	finishChain() {
+		let newArr = [].concat(this.chain)
+
+		this.chain = [];
+
+		for (let value of newArr) {
+			// if (newArr[0] == value) {
+			// 	this.chain.push(`( ${value} )`)
+			// 	console.log('ye');
+			// } else {
+			this.chain.push(`~~( ${value} )`)
+			this.chain.splice(0, 1);
+			this.chain.unshift(`( ${newArr[0]} )`)
+			// }
+		}
+
+
+		newArr = this.chain.join('')
+		this.chain = []
+		return newArr
+	}
 };
 
+// console.log(cha);
+//console.log(chainMaker.addLink(1).addLink(2).addLink(3).removeLink(0))
+//console.log(chainMaker.addLink(1).addLink(2).addLink(3).removeLink('2nd'))
+//console.log(chainMaker.addLink(1).addLink(2).addLink(3).removeLink(-2))
+console.log((chainMaker.reverseChain().addLink('ABC').reverseChain().reverseChain().reverseChain().addLink(Infinity).addLink(false).addLink(0).addLink('8.963').removeLink(2).removeLink(1).reverseChain().finishChain()))
+// ( 8.963 )~~( 0 )~~( false )
+
 module.exports = {
-  chainMaker
+	chainMaker
 };
--- src/transform-array.js
+++ src/transform-array.js
@@ -13,11 +13,75 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr, indx) {
+	if (!(arr instanceof Array)) {
+		throw new Error("'arr' parameter must be an instance of the Array!")
+	}
+	let newArr = arr.concat();
+	let index1
+	let index2
+	let indexRemem = indx
+	arr.map((item, index) => {
+
+		if (item === '--discard-next') {
+			index1 = index;
+			index2 = index + 1;
+			if (index1 == arr.length - 1) {
+				newArr.splice(index, 1)
+				return
+			}
+			newArr.splice(index1, 2);
+			indexRemem = index2;
+		}
+
+		if (item === '--discard-prev') {
+			index1 = index;
+			index2 = index - 1;
+			if (index1 == 0) {
+				newArr.splice(index, 1)
+				return
+			}
+			if (index2 === indexRemem && arr[indexRemem - 1] != '--double-next') {
+				let indexDelete = newArr.findIndex(item => item === arr[index]);
+				newArr.splice(indexDelete, 1)
+				return
+			}
+			newArr.splice(index2, 2)
+
+		}
+		if (item === '--double-next') {
+			index1 = index;
+			index2 = index + 1;
+			if (index1 == arr.length - 1) {
+				newArr.splice(index, 1)
+				return
+			}
+			newArr.splice(index1, 1)
+			newArr = [].concat(newArr.slice(0, index2), newArr.slice(index1, newArr.length))
+			indexRemem = index2;
+		}
+		if (item === '--double-prev') {
+			index1 = index;
+			index2 = index - 1;
+			console.log(index2);
+			console.log(indexRemem);
+			if (index1 == 0) {
+				newArr.splice(index, 1)
+				return
+			}
+			if (index2 === indexRemem && arr[indexRemem - 1] != '--double-next') {
+				let indexDelete = newArr.findIndex(item => item === arr[index]);
+				newArr.splice(indexDelete, 1)
+				return
+			}
+			newArr.splice(index1, 1)
+			newArr = [].concat(newArr.slice(0, index1), newArr.slice(index1 - 1, newArr.length))
+		}
+	})
+	return newArr
 }
 
+
 module.exports = {
-  transform
+	transform
 };
