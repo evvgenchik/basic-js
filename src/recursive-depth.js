4c7a3f81f965ca89c82d270d877d21bbc581dc91
Evgeniy <106548359+evvgenchik@users.noreply.github.com>
2022-10-16 16:27:17 +0300

    fix: git

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,17 +1,17 @@
-// const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require('../extensions/index.js');
 
-// /**
-//  * Implement class DepthCalculator with method calculateDepth
-//  * that calculates deoth of nested array
-//  * 
-//  * @example
-//  * 
-//  * const depthCalc = new DepthCalculator();
-//  * depthCalc.calculateDepth([1, 2, 3, 4, 5]) => 1
-//  * depthCalc.calculateDepth([1, 2, 3, [4, 5]]) => 2
-//  * depthCalc.calculateDepth([[[]]]) => 3
-//  *
-//  */
+/**
+ * Implement class DepthCalculator with method calculateDepth
+ * that calculates deoth of nested array
+ * 
+ * @example
+ * 
+ * const depthCalc = new DepthCalculator();
+ * depthCalc.calculateDepth([1, 2, 3, 4, 5]) => 1
+ * depthCalc.calculateDepth([1, 2, 3, [4, 5]]) => 2
+ * depthCalc.calculateDepth([[[]]]) => 3
+ *
+ */
 class DepthCalculator {
 
 	calculateDepth(arr) {
@@ -32,14 +32,7 @@ class DepthCalculator {
 
 
 
-const depthCalc = new DepthCalculator();
-// // // console.log(depthCalc.calculateDepth([1, 2, 3, 4, 5])) //1
-// console.log(depthCalc.calculateDepth(([1, [8, [[]]], [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[]]]]]]], []]]], []]]]]]]]], []]]], []]]]]]]]]], 2, 3, [8, [[[[[[[[[[[[[[]]]]]]]]]]]]]]], [8, [[[[[[[[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]]]]]]]]], 4, 5, ['6575', ['adas', ['dfg', [0]]]]]))) // 31
-// console.log(depthCalc.calculateDepth([1, 2, 3, 4, 5, [1]])) //2
-// console.log(depthCalc.calculateDepth([1, [8, [[]]], 2, 3, [8, []], 4, 5, ['6575', ['adas', ['dfg', [0]]]]])) //5
-console.log(depthCalc.calculateDepth([1, [8, [[]]], 2, 3, [8, [[[[[[[[[[[[[]]]]]]]]]]]]]], 4, 5, ['6575', ['adas', ['dfg', [0]]]]])) //15
 
-
-// module.exports = {
-// 	DepthCalculator
-// };
+module.exports = {
+	DepthCalculator
+};
